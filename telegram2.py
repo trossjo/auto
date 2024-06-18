5c89e557c8e574a80327c5499d94d9d7f42a1606
trossjo <joeulhan@gmail.com>
2024-06-18 23:00:52 +0900

    a

--- telegram2.py
+++ telegram2.py
@@ -14,34 +14,56 @@ while True:
 	싸움 = image_lib.match_img(hwnd, ".\\coin_img\\fight.jpg")
 	if 싸움:
 		check = 0
-		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg",0.98)
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
 		if 매크로종료:
 			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1])
-
-		image_lib.mouse_click(hwnd, 싸움[0], 싸움[1])
 		time.sleep(1)
-		매크로시작 = image_lib.match_img(hwnd, ".\\coin_img\\macro_start.jpg",0.98)
+		image_lib.mouse_click(hwnd, 싸움[0], 싸움[1])
+		매크로시작 = image_lib.match_img(hwnd, ".\\coin_img\\macro_start.jpg")
 		if 매크로시작:
 			image_lib.mouse_click(hwnd, 매크로시작[0], 매크로시작[1]-20)
 
 	새게임 = image_lib.match_img(hwnd, ".\\coin_img\\fight2.jpg")
 	if 새게임:
 		check = 0
-		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg",0.98)
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
 		if 매크로종료:
 			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1])
-
-		image_lib.mouse_click(hwnd, 새게임[0], 새게임[1])
 		time.sleep(1)
-		매크로시작 = image_lib.match_img(hwnd, ".\\coin_img\\macro_start.jpg",0.98)
+		image_lib.mouse_click(hwnd, 새게임[0], 새게임[1])
+		매크로시작 = image_lib.match_img(hwnd, ".\\coin_img\\macro_start.jpg")
 		if 매크로시작:
 			image_lib.mouse_click(hwnd, 매크로시작[0], 매크로시작[1]-20)
 
 	싸움탭 = image_lib.capture_click(hwnd, ".\\coin_img\\f_tab.jpg",0.98)
 	if 싸움탭:
-		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg",0.98)
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
 		if 매크로종료:
 			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1]-20)
 			image_lib.mouse_click(hwnd, 싸움탭[0], 싸움탭[1])
+
+	텔레그램 = image_lib.capture_click(hwnd, ".\\coin_img\\tel.jpg",0.98)
+	if 싸움탭:
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
+		if 매크로종료:
+			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1]-20)
+			time.sleep(1)
+			image_lib.mouse_click(hwnd, 텔레그램[0], 텔레그램[1])
+
+	텔레그램채팅 = image_lib.capture_click(hwnd, ".\\coin_img\\tel_chat.jpg",0.98)
+	if 싸움탭:
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
+		if 매크로종료:
+			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1]-20)
+			time.sleep(1)
+			image_lib.mouse_click(hwnd, 텔레그램채팅[0], 텔레그램채팅[1])
+
+	텔레그램채팅2 = image_lib.capture_click(hwnd, ".\\coin_img\\tel_chat2.jpg",0.98)
+	if 싸움탭:
+		매크로종료 = image_lib.match_img(hwnd, ".\\coin_img\\macro_stop.jpg")
+		if 매크로종료:
+			image_lib.mouse_click(hwnd, 매크로종료[0], 매크로종료[1]-20)
+			time.sleep(1)
+			image_lib.mouse_click(hwnd, 텔레그램채팅2[0], 텔레그램채팅2[1])
 
 	time.sleep(1)
